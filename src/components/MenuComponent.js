f42889d3c3bc74e2773d4706e1a3336a18e757d1
Albert4940 <dorcealbertmary@gmail.com>
2021-06-28 17:21:44 -0500

    Presentational and Container Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -7,31 +7,6 @@ class Menu extends Component {
 
   constructor(props) {
       super(props);
-
-      this.state = {
-          selectedDish: null
-      }
-  }
-
-  onDishSelect(dish) {
-      this.setState({ selectedDish: dish});
-  }
-
-  renderDish(dish) {
-      if (dish != null)
-          return(
-              <Card>
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-              </Card>
-          );
-      else
-          return(
-              <div></div>
-          );
   }
 
   render() {
@@ -39,7 +14,7 @@ class Menu extends Component {
           return (
             <div  className="col-12 col-md-5 m-1">
               <Card key={dish.id}
-                onClick={() => this.onDishSelect(dish)}>
+                onClick={() => this.props.onClick(dish.id)}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -53,12 +28,7 @@ class Menu extends Component {
           <div className="container">
               <div className="row">
                   {menu}
-              </div>
-              <div className="row">
-                <div  className="col-12 col-md-5 m-1">
-                  {this.renderDish(this.state.selectedDish)}
-                </div>
-              </div>
+              </div>  
           </div>
       );
   }
