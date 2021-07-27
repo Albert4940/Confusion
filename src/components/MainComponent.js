df6ea515898334caf43760e1ba8f248c6d0059cc
Albert4940 <dorcealbertmary@gmail.com>
2021-07-27 16:24:02 -0500

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,30 +1,46 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent.js'; 
 import {DISHES} from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { Component } from 'react';
 class Main extends Component {
   constructor(props){
     super(props)
     this.state = {
-        dishes:DISHES,
-        selectedDish: null
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
   
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-}
   render(){
+    const HomePage = () => {
+      return(
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
+    }
   return (
-    <div >
+    <div>
       <Header />
-      <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
-      <DishDetail 
-         dish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}
-      />
+      <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />
+              <Redirect to="/home" />
+          </Switch>
        <Footer />
     </div>
   );
